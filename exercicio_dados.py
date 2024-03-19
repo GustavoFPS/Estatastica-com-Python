98c17c9b84afc36625c422c2246d2b353696a3a8
Gustavo Freire <147093330+GustavoFPS@users.noreply.github.com>
2024-03-19 17:04:30 -0300

    nv1

--- exercicio_dados.py
+++ exercicio_dados.py
@@ -6,23 +6,15 @@ gen = np.random.default_rng()
 nlances = 1000
 ndados = 5
 dados = gen.integers(1, 6, size = (nlances, 5), endpoint= True)
-#dados = [[4, 4, 2 , 4, 4], [4, 4, 2 , 1, 4], [3, 2, 3 , 3, 3]]
+
 quadra = 0
 
 
 for jogada in dados:
-     
-    for element in jogada:
-        count = 0 
-        
-        for dado in range(0, 5):
-             
-            if element == jogada[dado]:
-                count += 1 
-        if count == 4: 
-            quadra += 1 
-            break
-     
-print(quadra)   
+    if 4 in np.unique(jogada, return_counts=True)[1]:
+        quadra += 1
+
+print(quadra)
+  
 
 print(quadra/1000)
